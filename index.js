bb68439ed85b0e9f39475c0ddf226706e303b64c
hascdev <ale.sandoval.ii@gmail.com>
2024-03-08 08:19:55 -0300

    feat: add vercel config

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-import "dotenv/config.js";
-import App from './app.js';
+import express from 'express';
+import AuthRouter from './routers/auth.router.js';
+import UserRouter from './routers/user.router.js';
 
-export default App.start(process.env.SERVER_PORT);
+const app = express(); 
+
+app.use(express.json());
+        
+// Less hackers know about our stack
+app.disable('x-powered-by');
+  
+app.get('/', (_, res) => {
+    res.json({ message: 'Welcome to our service!' });
+});
+
+app.use('/api/v1/auth', AuthRouter.getRouter());
+app.use('/api/v1/users', UserRouter.getRouter());
+  
+// export the app for vercel serverless functions 
+module.exports = app;
