f1142824bf871f656694606160a7c78ab6ce078e
hascdev <ale.sandoval.ii@gmail.com>
2024-03-08 08:27:07 -0300

    feat: add vercel config

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import AuthRouter from './routers/auth.router.js';
-import UserRouter from './routers/user.router.js';
 
 const app = express(); 
 
@@ -12,9 +10,6 @@ app.disable('x-powered-by');
 app.get('/', (_, res) => {
     res.json({ message: 'Welcome to our service!' });
 });
-
-app.use('/api/v1/auth', AuthRouter.getRouter());
-app.use('/api/v1/users', UserRouter.getRouter());
   
 // export the app for vercel serverless functions 
-module.exports = app;
+export default app;
